92319362eeb93b4ee473e7a94c44234184a3f86b
enderrazer <enderrazer@mail.ru>
2024-08-17 13:10:36 +0300

    Updates

--- assets/List-Bc6yjOVU.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,b as r,j as i,c as l,d as p,e as d}from"./index-CG1W8DfO.js";const f=(...a)=>(...e)=>a.filter(s=>typeof s=="function").forEach(s=>s(...e)),m=a=>{var{className:e,children:s}=a,t=o(a,["className","children"]);const c=r();return i.jsx("div",l(p({className:d("tgui-389a43acd684137a",c==="ios"&&"tgui-cfed40fe81d34ad5",e)},t),{children:s}))};export{m as L,f as c};
